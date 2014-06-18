d38bed71c073340bef1c1a014023fc949237e91d
alojzije <shljunak@hotmail.com>
2014-06-18 18:19:27 +0200

    Make ball not fly up for testi9ng purposes

--- scripts/mover/predefineMovers.js
+++ scripts/mover/predefineMovers.js
@@ -2,8 +2,8 @@ function addPredefinedMovers(){
     var height = $(window).height();
     var width  = $(window).width();
     //uncomment now!
-    var m0 = makeCircle('circle', new Vector2D(0, height-10), new Vector2D(-0.004*width,-0.015*height));
-    //var m0 = makeCircle('circle', new Vector2D(width-400,height-5), new Vector2D(0,0));
+    //var m0 = makeCircle('circle', new Vector2D(0, height-10), new Vector2D(-0.004*width,-0.015*height));
+    var m0 = makeCircle('circle', new Vector2D(width-400,height-5), new Vector2D(0,0));
 
     m0.mass = 1.4;
     animateGravity(m0); 
